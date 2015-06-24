3684f4a8cdabdffcfbcc9a9ea039d417c716f211
jirisemecky <jiri.semecky@gmail.com>
2015-06-25 00:32:37 +0200

    Initial Java implementation.

---
+++ java/codes/plus/tests/PrecisionTest.java
@@ -0,0 +1,31 @@
+package codes.plus.tests;
+
+import codes.plus.OpenLocationCode;
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Tests size of rectangles defined by open location codes of various size.
+ */
+public class PrecisionTest {
+
+    @Test
+    public void testWidthInDegrees() {
+        Assert.assertEquals(new OpenLocationCode("67000000+").decode().getLongitudeWidth(), 20.);
+        Assert.assertEquals(new OpenLocationCode("67890000+").decode().getLongitudeWidth(), 1.);
+        Assert.assertEquals(new OpenLocationCode("6789CF00+").decode().getLongitudeWidth(), 0.05);
+        Assert.assertEquals(new OpenLocationCode("6789CFGH+").decode().getLongitudeWidth(), 0.0025);
+        Assert.assertEquals(new OpenLocationCode("6789CFGH+JM").decode().getLongitudeWidth(), 0.000125);
+        Assert.assertEquals(new OpenLocationCode("6789CFGH+JMP").decode().getLongitudeWidth(), 0.00003125);
+    }
+
+    @Test
+    public void testHeightInDegrees() {
+        Assert.assertEquals(new OpenLocationCode("67000000+").decode().getLatitudeHeight(), 20.);
+        Assert.assertEquals(new OpenLocationCode("67890000+").decode().getLatitudeHeight(), 1.);
+        Assert.assertEquals(new OpenLocationCode("6789CF00+").decode().getLatitudeHeight(), 0.05);
+        Assert.assertEquals(new OpenLocationCode("6789CFGH+").decode().getLatitudeHeight(), 0.0025);
+        Assert.assertEquals(new OpenLocationCode("6789CFGH+JM").decode().getLatitudeHeight(), 0.000125);
+        Assert.assertEquals(new OpenLocationCode("6789CFGH+JMP").decode().getLatitudeHeight(), 0.000025);
+    }
+}
---
+++ java/codes/plus/tests/ShorteningTest.java
@@ -0,0 +1,64 @@
+package codes.plus.tests;
+
+import codes.plus.OpenLocationCode;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/** Tests shortening functionality of Open Location Code. */
+public class ShorteningTest {
+    private class TestData {
+        private final String code;
+        private final double referenceLatitude;
+        private final double referenceLongitude;
+        private final String shortCode;
+
+        public TestData(String line) {
+            String[] parts = line.split(",");
+            if (parts.length != 4) throw new IllegalArgumentException("Wrong format of testing data.");
+            this.code = parts[0];
+            this.referenceLatitude = Double.valueOf(parts[1]);
+            this.referenceLongitude = Double.valueOf(parts[2]);
+            this.shortCode = parts[3];
+        }
+    }
+
+    private final List<TestData> testDataList = new ArrayList<>();
+
+    @Before
+    public void setUp() throws Exception {
+        InputStream testDataStream = ClassLoader.getSystemResourceAsStream("shortCodeTests.csv");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(testDataStream));
+        String line;
+        while((line = reader.readLine()) != null) {
+            if (line.startsWith("#")) continue;
+            testDataList.add(new TestData(line));
+        }
+    }
+
+    @Test
+    public void testShortening() {
+        for (TestData testData : testDataList) {
+            OpenLocationCode olc = new OpenLocationCode(testData.code);
+            OpenLocationCode shortened = olc.shorten(testData.referenceLatitude, testData.referenceLongitude);
+            Assert.assertEquals("Wrong shortening of code " + testData.code,
+                testData.shortCode,
+                shortened.getCode());
+        }
+    }
+
+    @Test
+    public void testRecovering() {
+        for (TestData testData : testDataList) {
+            OpenLocationCode olc = new OpenLocationCode(testData.shortCode);
+            OpenLocationCode recovered = olc.recover(testData.referenceLatitude, testData.referenceLongitude);
+            Assert.assertEquals(testData.code, recovered.getCode());
+        }
+    }
+}
