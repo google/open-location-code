1c15d2f7c40f0d7923d38a1a80a975089a7a3d68
Doug Rinckes <drinckes@google.com>
2015-08-19 15:56:51 +0200

    update move of test file

--- js/gulpfile.js
+++ js/gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp'),
     qunit = require('gulp-qunit');
 
 gulp.task('default', function() {
-    return gulp.src('./test.html')
+    return gulp.src('./test/test.html')
         .pipe(qunit());
 });
