c9affd5f91b89b36be2509751e9440fcb29b5df3
jirisemecky <jiri.semecky@gmail.com>
2015-09-22 23:58:36 +0200

    Adding precision parameter to asserts.

--- java/codes/plus/tests/PrecisionTest.java
+++ java/codes/plus/tests/PrecisionTest.java
@@ -11,21 +11,21 @@ public class PrecisionTest {
 
     @Test
     public void testWidthInDegrees() {
-        Assert.assertEquals(new OpenLocationCode("67000000+").decode().getLongitudeWidth(), 20.);
-        Assert.assertEquals(new OpenLocationCode("67890000+").decode().getLongitudeWidth(), 1.);
-        Assert.assertEquals(new OpenLocationCode("6789CF00+").decode().getLongitudeWidth(), 0.05);
-        Assert.assertEquals(new OpenLocationCode("6789CFGH+").decode().getLongitudeWidth(), 0.0025);
-        Assert.assertEquals(new OpenLocationCode("6789CFGH+JM").decode().getLongitudeWidth(), 0.000125);
-        Assert.assertEquals(new OpenLocationCode("6789CFGH+JMP").decode().getLongitudeWidth(), 0.00003125);
+        Assert.assertEquals(new OpenLocationCode("67000000+").decode().getLongitudeWidth(), 20., 0);
+        Assert.assertEquals(new OpenLocationCode("67890000+").decode().getLongitudeWidth(), 1., 0);
+        Assert.assertEquals(new OpenLocationCode("6789CF00+").decode().getLongitudeWidth(), 0.05, 0);
+        Assert.assertEquals(new OpenLocationCode("6789CFGH+").decode().getLongitudeWidth(), 0.0025, 0);
+        Assert.assertEquals(new OpenLocationCode("6789CFGH+JM").decode().getLongitudeWidth(), 0.000125, 0);
+        Assert.assertEquals(new OpenLocationCode("6789CFGH+JMP").decode().getLongitudeWidth(), 0.00003125, 0);
     }
 
     @Test
     public void testHeightInDegrees() {
-        Assert.assertEquals(new OpenLocationCode("67000000+").decode().getLatitudeHeight(), 20.);
-        Assert.assertEquals(new OpenLocationCode("67890000+").decode().getLatitudeHeight(), 1.);
-        Assert.assertEquals(new OpenLocationCode("6789CF00+").decode().getLatitudeHeight(), 0.05);
-        Assert.assertEquals(new OpenLocationCode("6789CFGH+").decode().getLatitudeHeight(), 0.0025);
-        Assert.assertEquals(new OpenLocationCode("6789CFGH+JM").decode().getLatitudeHeight(), 0.000125);
-        Assert.assertEquals(new OpenLocationCode("6789CFGH+JMP").decode().getLatitudeHeight(), 0.000025);
+        Assert.assertEquals(new OpenLocationCode("67000000+").decode().getLatitudeHeight(), 20., 0);
+        Assert.assertEquals(new OpenLocationCode("67890000+").decode().getLatitudeHeight(), 1., 0);
+        Assert.assertEquals(new OpenLocationCode("6789CF00+").decode().getLatitudeHeight(), 0.05, 0);
+        Assert.assertEquals(new OpenLocationCode("6789CFGH+").decode().getLatitudeHeight(), 0.0025, 0);
+        Assert.assertEquals(new OpenLocationCode("6789CFGH+JM").decode().getLatitudeHeight(), 0.000125, 0);
+        Assert.assertEquals(new OpenLocationCode("6789CFGH+JMP").decode().getLatitudeHeight(), 0.000025, 0);
     }
 }
