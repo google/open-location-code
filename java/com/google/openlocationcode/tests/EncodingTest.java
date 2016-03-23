a77ef17450ad5c85eac611fe95c555eeb1954e8f
Zongwei Li <zongwei@google.com>
2016-03-23 13:07:53 -0700

    Convert public constructor to public static factory method and format code to Google Java Style Guide standards.

--- java/com/google/openlocationcode/tests/EncodingTest.java
+++ java/com/google/openlocationcode/tests/EncodingTest.java
@@ -1,6 +1,7 @@
 package com.google.openlocationcode.tests;
 
 import com.google.openlocationcode.OpenLocationCode;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,117 +17,130 @@ import java.util.List;
  */
 public class EncodingTest {
 
-    public static final double PRECISION = 1e-10;
+  public static final double PRECISION = 1e-10;
 
-    private class TestData {
-        private final String code;
-        private final double latitude;
-        private final double longitude;
-        private final double decodedLatitudeLo;
-        private final double decodedLatitudeHi;
-        private final double decodedLongitudeLo;
-        private final double decodedLongitudeHi;
+  private class TestData {
 
-        public TestData(String line) {
-            String[] parts = line.split(",");
-            if (parts.length != 7) throw new IllegalArgumentException("Wrong format of testing data.");
-            this.code = parts[0];
-            this.latitude = Double.valueOf(parts[1]);
-            this.longitude = Double.valueOf(parts[2]);
-            this.decodedLatitudeLo = Double.valueOf(parts[3]);
-            this.decodedLongitudeLo = Double.valueOf(parts[4]);
-            this.decodedLatitudeHi = Double.valueOf(parts[5]);
-            this.decodedLongitudeHi = Double.valueOf(parts[6]);
-        }
+    private final String code;
+    private final double latitude;
+    private final double longitude;
+    private final double decodedLatitudeLo;
+    private final double decodedLatitudeHi;
+    private final double decodedLongitudeLo;
+    private final double decodedLongitudeHi;
+
+    public TestData(String line) {
+      String[] parts = line.split(",");
+      if (parts.length != 7) {
+        throw new IllegalArgumentException("Wrong format of testing data.");
+      }
+      this.code = parts[0];
+      this.latitude = Double.valueOf(parts[1]);
+      this.longitude = Double.valueOf(parts[2]);
+      this.decodedLatitudeLo = Double.valueOf(parts[3]);
+      this.decodedLongitudeLo = Double.valueOf(parts[4]);
+      this.decodedLatitudeHi = Double.valueOf(parts[5]);
+      this.decodedLongitudeHi = Double.valueOf(parts[6]);
     }
+  }
 
-    private final List<TestData> testDataList = new ArrayList<>();
+  private final List<TestData> testDataList = new ArrayList<>();
 
-    @Before
-    public void setUp() throws Exception {
-        InputStream testDataStream = ClassLoader.getSystemResourceAsStream("encodingTests.csv");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(testDataStream));
-        String line;
-        while((line = reader.readLine()) != null) {
-            if (line.startsWith("#")) continue;
-            testDataList.add(new TestData(line));
-        }
+  @Before
+  public void setUp() throws Exception {
+    InputStream testDataStream = ClassLoader.getSystemResourceAsStream("encodingTests.csv");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(testDataStream));
+    String line;
+    while ((line = reader.readLine()) != null) {
+      if (line.startsWith("#")) {
+        continue;
+      }
+      testDataList.add(new TestData(line));
     }
+  }
 
-    @Test
-    public void testEncodeFromLatLong() {
-        for (TestData testData : testDataList) {
-            int codeLength = testData.code.length() - 1;
-            if (testData.code.contains("0")) {
-                codeLength = testData.code.indexOf("0");
-            }
-            Assert.assertEquals(
-                "Latitude " + testData.latitude + " and longitude " + testData.longitude +""
-                    + " were wrongly encoded.",
-                testData.code,
-                OpenLocationCode.encode(testData.latitude, testData.longitude, codeLength).toString());
-        }
+  @Test
+  public void testEncodeFromLatLong() {
+    for (TestData testData : testDataList) {
+      int codeLength = testData.code.length() - 1;
+      if (testData.code.contains("0")) {
+        codeLength = testData.code.indexOf("0");
+      }
+      Assert.assertEquals(
+          String.format(
+              "Latitude %f and longitude %f were wrongly encoded.",
+              testData.latitude,
+              testData.longitude),
+          testData.code,
+          OpenLocationCode.encode(testData.latitude, testData.longitude, codeLength).toString());
     }
+  }
 
-    @Test
-    public void testDecode() {
-        for (TestData testData : testDataList) {
-            OpenLocationCode.CodeArea decoded = new OpenLocationCode(testData.code).decode();
+  @Test
+  public void testDecode() {
+    for (TestData testData : testDataList) {
+      OpenLocationCode.CodeArea decoded = OpenLocationCode.of(testData.code).decode();
 
-            Assert.assertEquals("Wrong low latitude for code " + testData.code,
-                testData.decodedLatitudeLo,
-                decoded.getSouthLatitude(),
-                PRECISION);
-            Assert.assertEquals("Wrong high latitude for code " + testData.code,
-                testData.decodedLatitudeHi,
-                decoded.getNorthLatitude(),
-                PRECISION);
+      Assert.assertEquals("Wrong low latitude for code " + testData.code,
+          testData.decodedLatitudeLo,
+          decoded.getSouthLatitude(),
+          PRECISION);
+      Assert.assertEquals("Wrong high latitude for code " + testData.code,
+          testData.decodedLatitudeHi,
+          decoded.getNorthLatitude(),
+          PRECISION);
 
-            Assert.assertEquals("Wrong low longitude for code " + testData.code,
-                testData.decodedLongitudeLo,
-                decoded.getWestLongitude(),
-                PRECISION);
-            Assert.assertEquals("Wrong high longitude for code " + testData.code,
-                testData.decodedLongitudeHi,
-                decoded.getEastLongitude(),
-                PRECISION);
-        }
+      Assert.assertEquals("Wrong low longitude for code " + testData.code,
+          testData.decodedLongitudeLo,
+          decoded.getWestLongitude(),
+          PRECISION);
+      Assert.assertEquals("Wrong high longitude for code " + testData.code,
+          testData.decodedLongitudeHi,
+          decoded.getEastLongitude(),
+          PRECISION);
     }
+  }
 
-    @Test
-    public void testClipping() {
-        Assert.assertEquals("Clipping of negative latitude doesn't work.",
-            OpenLocationCode.encode(-90, 5),
-            OpenLocationCode.encode(-91, 5));
-        Assert.assertEquals("Clipping of positive latitude doesn't work.",
-            OpenLocationCode.encode(90, 5),
-            OpenLocationCode.encode(91, 5));
-        Assert.assertEquals("Clipping of negative longitude doesn't work.",
-            OpenLocationCode.encode(5, 175),
-            OpenLocationCode.encode(5, -185));
-        Assert.assertEquals("Clipping of very long negative longitude doesn't work.",
-            OpenLocationCode.encode(5, 175),
-            OpenLocationCode.encode(5, -905));
-        Assert.assertEquals("Clipping of very long positive longitude doesn't work.",
-            OpenLocationCode.encode(5, -175),
-            OpenLocationCode.encode(5, 905));
-    }
+  @Test
+  public void testClipping() {
+    Assert.assertEquals("Clipping of negative latitude doesn't work.",
+        OpenLocationCode.encode(-90, 5),
+        OpenLocationCode.encode(-91, 5));
+    Assert.assertEquals("Clipping of positive latitude doesn't work.",
+        OpenLocationCode.encode(90, 5),
+        OpenLocationCode.encode(91, 5));
+    Assert.assertEquals("Clipping of negative longitude doesn't work.",
+        OpenLocationCode.encode(5, 175),
+        OpenLocationCode.encode(5, -185));
+    Assert.assertEquals("Clipping of very long negative longitude doesn't work.",
+        OpenLocationCode.encode(5, 175),
+        OpenLocationCode.encode(5, -905));
+    Assert.assertEquals("Clipping of very long positive longitude doesn't work.",
+        OpenLocationCode.encode(5, -175),
+        OpenLocationCode.encode(5, 905));
+  }
 
-    @Test
-    public void testContains() {
-        for (TestData testData : testDataList) {
-            OpenLocationCode olc = new OpenLocationCode(testData.code);
-            OpenLocationCode.CodeArea decoded = olc.decode();
-            Assert.assertTrue("Containment relation is broken for the decoded middle point of code " + testData.code,
-                olc.contains(decoded.getCenterLatitude(), decoded.getCenterLongitude()));
-            Assert.assertTrue("Containment relation is broken for the decoded bottom left corner of code " + testData.code,
-                olc.contains(decoded.getSouthLatitude(), decoded.getWestLongitude()));
-            Assert.assertFalse("Containment relation is broken for the decoded top right corner of code " + testData.code,
-                olc.contains(decoded.getNorthLatitude(), decoded.getEastLongitude()));
-            Assert.assertFalse("Containment relation is broken for the decoded bottom right corner of code " + testData.code,
-                olc.contains(decoded.getSouthLatitude(), decoded.getEastLongitude()));
-            Assert.assertFalse("Containment relation is broken for the decoded top left corner of code " + testData.code,
-                olc.contains(decoded.getNorthLatitude(), decoded.getWestLongitude()));
-        }
+  @Test
+  public void testContains() {
+    for (TestData testData : testDataList) {
+      OpenLocationCode olc = OpenLocationCode.of(testData.code);
+      OpenLocationCode.CodeArea decoded = olc.decode();
+      Assert.assertTrue(
+          "Containment relation is broken for the decoded middle point of code " + testData.code,
+          olc.contains(decoded.getCenterLatitude(), decoded.getCenterLongitude()));
+      Assert.assertTrue("Containment relation is broken for the decoded bottom left corner of code "
+          + testData.code,
+          olc.contains(decoded.getSouthLatitude(), decoded.getWestLongitude()));
+      Assert.assertFalse("Containment relation is broken for the decoded top right corner of code "
+          + testData.code,
+          olc.contains(decoded.getNorthLatitude(), decoded.getEastLongitude()));
+      Assert.assertFalse(
+          "Containment relation is broken for the decoded bottom right corner of code "
+              + testData.code,
+          olc.contains(decoded.getSouthLatitude(), decoded.getEastLongitude()));
+      Assert.assertFalse(
+          "Containment relation is broken for the decoded top left corner of code " + testData.code,
+          olc.contains(decoded.getNorthLatitude(), decoded.getWestLongitude()));
     }
+  }
 }
--- java/com/google/openlocationcode/tests/PrecisionTest.java
+++ java/com/google/openlocationcode/tests/PrecisionTest.java
@@ -1,6 +1,7 @@
 package com.google.openlocationcode.tests;
 
 import com.google.openlocationcode.OpenLocationCode;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,23 +10,27 @@ import org.junit.Test;
  */
 public class PrecisionTest {
 
-    @Test
-    public void testWidthInDegrees() {
-        Assert.assertEquals(new OpenLocationCode("67000000+").decode().getLongitudeWidth(), 20., 0);
-        Assert.assertEquals(new OpenLocationCode("67890000+").decode().getLongitudeWidth(), 1., 0);
-        Assert.assertEquals(new OpenLocationCode("6789CF00+").decode().getLongitudeWidth(), 0.05, 0);
-        Assert.assertEquals(new OpenLocationCode("6789CFGH+").decode().getLongitudeWidth(), 0.0025, 0);
-        Assert.assertEquals(new OpenLocationCode("6789CFGH+JM").decode().getLongitudeWidth(), 0.000125, 0);
-        Assert.assertEquals(new OpenLocationCode("6789CFGH+JMP").decode().getLongitudeWidth(), 0.00003125, 0);
-    }
+  @Test
+  public void testWidthInDegrees() {
+    Assert.assertEquals(OpenLocationCode.of("67000000+").decode().getLongitudeWidth(), 20., 0);
+    Assert.assertEquals(OpenLocationCode.of("67890000+").decode().getLongitudeWidth(), 1., 0);
+    Assert.assertEquals(OpenLocationCode.of("6789CF00+").decode().getLongitudeWidth(), 0.05, 0);
+    Assert.assertEquals(OpenLocationCode.of("6789CFGH+").decode().getLongitudeWidth(), 0.0025, 0);
+    Assert.assertEquals(
+        OpenLocationCode.of("6789CFGH+JM").decode().getLongitudeWidth(), 0.000125, 0);
+    Assert.assertEquals(
+        OpenLocationCode.of("6789CFGH+JMP").decode().getLongitudeWidth(), 0.00003125, 0);
+  }
 
-    @Test
-    public void testHeightInDegrees() {
-        Assert.assertEquals(new OpenLocationCode("67000000+").decode().getLatitudeHeight(), 20., 0);
-        Assert.assertEquals(new OpenLocationCode("67890000+").decode().getLatitudeHeight(), 1., 0);
-        Assert.assertEquals(new OpenLocationCode("6789CF00+").decode().getLatitudeHeight(), 0.05, 0);
-        Assert.assertEquals(new OpenLocationCode("6789CFGH+").decode().getLatitudeHeight(), 0.0025, 0);
-        Assert.assertEquals(new OpenLocationCode("6789CFGH+JM").decode().getLatitudeHeight(), 0.000125, 0);
-        Assert.assertEquals(new OpenLocationCode("6789CFGH+JMP").decode().getLatitudeHeight(), 0.000025, 0);
-    }
+  @Test
+  public void testHeightInDegrees() {
+    Assert.assertEquals(OpenLocationCode.of("67000000+").decode().getLatitudeHeight(), 20., 0);
+    Assert.assertEquals(OpenLocationCode.of("67890000+").decode().getLatitudeHeight(), 1., 0);
+    Assert.assertEquals(OpenLocationCode.of("6789CF00+").decode().getLatitudeHeight(), 0.05, 0);
+    Assert.assertEquals(OpenLocationCode.of("6789CFGH+").decode().getLatitudeHeight(), 0.0025, 0);
+    Assert.assertEquals(
+        OpenLocationCode.of("6789CFGH+JM").decode().getLatitudeHeight(), 0.000125, 0);
+    Assert.assertEquals(
+        OpenLocationCode.of("6789CFGH+JMP").decode().getLatitudeHeight(), 0.000025, 0);
+  }
 }
