77685cde30d3e7b59e147acf9df50a1ecd87b5e3
James Fysh <james.fysh@gmail.com>
2017-04-17 05:52:21 +1000

    Initial rust implementation
    working implementation (passes all validation, enc/dec, shorten/recover
    unit-tests); probably room for improvement wrt comments, layout.

---
+++ rust/src/private.rs
@@ -0,0 +1,74 @@
+use consts::{
+    CODE_ALPHABET, ENCODING_BASE, LATITUDE_MAX, LONGITUDE_MAX, PAIR_CODE_LENGTH, GRID_ROWS,
+    GRID_COLUMNS,
+};
+
+use interface::encode;
+
+use geo::Point;
+
+pub fn code_value(chr: char) -> usize {
+    for (i, c) in CODE_ALPHABET.iter().enumerate() {
+        if chr == *c {
+            return i;
+        }
+    }
+    0
+}
+
+pub fn normalize_longitude(value: f64) -> f64 {
+    let mut result: f64 = value;
+    while result >= LONGITUDE_MAX {
+        result = result - LONGITUDE_MAX * 2f64;
+    }
+    while result < -LONGITUDE_MAX{
+        result = result + LONGITUDE_MAX * 2f64;
+    }
+    result
+}
+
+pub fn clip_latitude(latitude_degrees: f64) -> f64 {
+    latitude_degrees.min(LATITUDE_MAX).max(-LATITUDE_MAX)
+}
+
+pub fn compute_latitude_precision(code_length: usize) -> f64 {
+    if code_length <= PAIR_CODE_LENGTH {
+        return ENCODING_BASE.powf((code_length as f64 / -2f64 + 2f64).floor())
+    }
+    ENCODING_BASE.powf(-3f64) / GRID_ROWS.powf(code_length as f64 - PAIR_CODE_LENGTH as f64)
+}
+
+pub fn prefix_by_reference(lat: f64, lng: f64, code_length: usize) -> String {
+    let precision = compute_latitude_precision(code_length);
+    let rounded_lat = (lat / precision).floor() * precision;
+    let rounded_lng = (lng / precision).floor() * precision;
+    let mut code = encode(
+        Point::new(rounded_lng, rounded_lat),
+        PAIR_CODE_LENGTH
+    );
+    code.drain(code_length..);
+    code
+}
+
+pub fn near(value: f64) -> bool {
+    value.trunc() != (value + 0.0000000001f64).trunc()
+}
+
+pub fn narrow_region(digit: usize, lat: &mut f64, lng: &mut f64) {
+    if digit == 0 {
+        *lat /= ENCODING_BASE;
+        *lng /= ENCODING_BASE;
+    } else if digit < 10 {
+        *lat *= ENCODING_BASE;
+        *lng *= ENCODING_BASE;
+    } else {
+        *lat *= GRID_ROWS;
+        *lng *= GRID_COLUMNS
+    }
+    if near(*lat) {
+        *lat = lat.round();
+    }
+    if near(*lng) {
+        *lng = lng.round();
+    }
+}
---
+++ rust/tests/all_test.rs
@@ -0,0 +1,88 @@
+extern crate olc;
+extern crate geo;
+
+use std::vec::Vec;
+
+use olc::{is_valid, is_short, is_full};
+use olc::{encode, decode};
+use olc::{shorten, recover_nearest};
+
+use geo::Point;
+
+mod csv_reader;
+
+use csv_reader::CSVReader;
+
+/// CSVReader is written to swallow errors; as such, we might "pass" tests because we didn't
+/// actually run any!  Thus, we use 'tested' below to count # lines read and hence to assert that
+/// > 0 tests were executed.
+///
+/// We could probably take it a little further, and assert that tested was >= # tests in the file
+/// (allowing tests to be added, but assuming # tests will never be reduced).
+
+#[test]
+fn is_valid_test() {
+    let mut tested = 0;
+    for line in CSVReader::new("validityTests.csv") {
+        let cols: Vec<&str> = line.split(',').collect();
+        let code = cols[0];
+        let _valid = cols[1] == "true";
+        let _short = cols[2] == "true";
+        let _full = cols[3] == "true";
+
+        assert_eq!(is_valid(code), _valid, "valid for code: {}", code);
+        assert_eq!(is_short(code), _short, "short for code: {}", code);
+        assert_eq!(is_full(code), _full, "full for code: {}", code);
+
+        tested += 1;
+    }
+    assert!(tested > 0);
+}
+
+#[test]
+fn encode_decode_test() {
+    let mut tested = 0;
+    for line in CSVReader::new("encodingTests.csv") {
+        let cols: Vec<&str> = line.split(',').collect();
+        let code = cols[0];
+        let lat = cols[1].parse::<f64>().unwrap();
+        let lng = cols[2].parse::<f64>().unwrap();
+        let latlo = cols[3].parse::<f64>().unwrap();
+        let lnglo = cols[4].parse::<f64>().unwrap();
+        let lathi = cols[5].parse::<f64>().unwrap();
+        let lnghi = cols[6].parse::<f64>().unwrap();
+
+        let codearea = decode(code).unwrap();
+        assert_eq!(
+            encode(Point::new(lng, lat), codearea.code_length),
+            code,
+            "encoding lat={},lng={}", lat, lng
+        );
+        assert!((latlo - codearea.south).abs() < 0.001f64);
+        assert!((lathi - codearea.north).abs() < 0.001f64);
+        assert!((lnglo - codearea.west).abs() < 0.001f64);
+        assert!((lnghi - codearea.east).abs() < 0.001f64);
+
+        tested += 1;
+    }
+    assert!(tested > 0);
+}
+
+#[test]
+fn shorten_recovery_test() {
+    let mut tested = 0;
+    for line in CSVReader::new("shortCodeTests.csv") {
+        let cols: Vec<&str> = line.split(',').collect();
+        let full_code = cols[0];
+        let lat = cols[1].parse::<f64>().unwrap();
+        let lng = cols[2].parse::<f64>().unwrap();
+        let short_code = cols[3];
+
+        assert_eq!(shorten(full_code, lat, lng).unwrap(), short_code, "shorten");
+        assert_eq!(recover_nearest(short_code, lat, lng), Ok(full_code.to_string()), "recover");
+
+        tested += 1;
+    }
+
+    assert!(tested > 0);
+}
