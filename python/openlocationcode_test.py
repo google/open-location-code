9b548ec3c76a081af5d992ce2c1849f1bf9d0d51
Isaac J <joh12041@umn.edu>
2017-06-30 14:37:29 -0500

    automated test cases

--- python/openlocationcode_test.py
+++ python/openlocationcode_test.py
@@ -1,53 +1,90 @@
-import pluscodes
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
 import unittest
+import openlocationcode as olc
 
-class TestValalidity(unittest.TestCase):
-    def test_validfullcodes(self):
-        self.assertTrue(pluscodes.isValid('8FWC2345+G6') and pluscodes.isFull('8FWC2345+G6'))
-        self.assertTrue(pluscodes.isValid('8FWC2345+G6G') and pluscodes.isFull('8FWC2345+G6G'))
-        self.assertTrue(pluscodes.isValid('8fwc2345+') and pluscodes.isFull('8fwc2345+'))
-        self.assertTrue(pluscodes.isValid('8FWCX400+') and pluscodes.isFull('8FWCX400+'))
-        self.assertFalse(pluscodes.isShort('8FWCX400+'))
-        self.assertFalse(pluscodes.isShort('8fwc2345+'))
+class TestValidity(unittest.TestCase):
+    def setUp(self):
+        self.testdata = []
+        headermap = {0: 'code', 1: 'isValid', 2: 'isShort', 3: 'isFull'}
+        tests_fn = 'test_data/validityTests.csv'
+        with open(tests_fn, "r") as fin:
+            for line in fin:
+                if line.startswith('#'):
+                    continue
+                td = line.strip().split(',')
+                assert len(td) == len(headermap), 'Wrong format of testing data: {0}'.format(line)
+                # all values should be booleans except the code
+                for i in range(1, len(headermap)):
+                    td[i] = (td[i] == 'true')
+                self.testdata.append({headermap[i]: v for i, v in enumerate(td)})
 
-    def test_validshortcodes(self):
-        self.assertTrue(pluscodes.isValid('WC2345+G6g') and pluscodes.isShort('WC2345+G6g'))
-        self.assertTrue(pluscodes.isValid('2345+G6') and pluscodes.isShort('2345+G6'))
-        self.assertTrue(pluscodes.isValid('45+G6') and pluscodes.isShort('45+G6'))
-        self.assertTrue(pluscodes.isValid('+G6') and pluscodes.isShort('+G6'))
-        self.assertFalse(pluscodes.isFull('45+G6'))
-        self.assertFalse(pluscodes.isFull('WC2345+G6g'))
+    def test_validcodes(self):
+        for td in self.testdata:
+            self.assertEqual(olc.isValid(td['code']), td['isValid'], td)
 
-    def test_invalidcodes(self):
-        self.assertFalse(pluscodes.isValid('G+'))
-        self.assertFalse(pluscodes.isValid('+'))
-        self.assertFalse(pluscodes.isValid('8FWC2345+G'))
-        self.assertFalse(pluscodes.isValid('8FWC2_45+G6'))
-        self.assertFalse(pluscodes.isValid('8FWC2η45+G6'))
-        self.assertFalse(pluscodes.isValid('8FWC2345+G6+'))
-        self.assertFalse(pluscodes.isValid('8FWC2300+G6'))
-        self.assertFalse(pluscodes.isValid('WC2300+G6g'))
-        self.assertFalse(pluscodes.isValid('WC2345+G'))
+    def test_fullcodes(self):
+        for td in self.testdata:
+            self.assertEqual(olc.isFull(td['code']), td['isFull'], td)
+
+    def test_shortcodes(self):
+        for td in self.testdata:
+            self.assertEqual(olc.isShort(td['code']), td['isShort'], td)
+
 
 class TestShorten(unittest.TestCase):
-    def test_full2short(self):
-        self.assertEqual('+2VX', pluscodes.shorten('9C3W9QCJ+2VX',51.3701125,-1.217765625))
-        self.assertEqual('CJ+2VX', pluscodes.shorten('9C3W9QCJ+2VX',51.3708675,-1.217765625))
-        self.assertEqual('CJ+2VX', pluscodes.shorten('9C3W9QCJ+2VX',51.3701125,-1.217010625))
-        self.assertEqual('9QCJ+2VX', pluscodes.shorten('9C3W9QCJ+2VX',51.3852125,-1.217765625))
+    def setUp(self):
+        self.testdata = []
+        headermap = {0: 'fullcode', 1: 'lat', 2: 'lng', 3: 'shortcode'}
+        tests_fn = 'test_data/shortCodeTests.csv'
+        with open(tests_fn, "r") as fin:
+            for line in fin:
+                if line.startswith('#'):
+                    continue
+                td = line.strip().split(',')
+                assert len(td) == len(headermap), 'Wrong format of testing data: {0}'.format(line)
+                td[1] = float(td[1])
+                td[2] = float(td[2])
+                self.testdata.append({headermap[i]: v for i, v in enumerate(td)})
 
-class TestEncode(unittest.TestCase):
-    def test_encode(self):
-        self.assertEqual('7FG49Q00+', pluscodes.encode(20.375,2.775,6))
-        self.assertEqual('7FG49QCJ+2V', pluscodes.encode(20.3700625,2.7821875))
-        self.assertEqual('7FG49QCJ+2VX', pluscodes.encode(20.3701125,2.782234375,11))
-        self.assertEqual('7FG49QCJ+2VXGJ', pluscodes.encode(20.3701135,2.78223535156,13))
-        self.assertEqual('8FVC2222+22', pluscodes.encode(47.0,8.0))
-        self.assertEqual('4VCPPQGP+Q9', pluscodes.encode(-41.2730625,174.7859375))
-        self.assertEqual('62G20000+', pluscodes.encode(0.5,-179.5,4))
-        self.assertEqual('22220000+', pluscodes.encode(-90,-180,4))
-        self.assertEqual('CFX30000+', pluscodes.encode(90,1,4))
-        self.assertEqual('62H20000+', pluscodes.encode(1,180,4))
+    def test_full2short(self):
+        for td in self.testdata:
+            self.assertEqual(td['shortcode'], olc.shorten(td['fullcode'], td['lat'], td['lng']), td)
+
+
+class TestEncoding(unittest.TestCase):
+    def setUp(self):
+        self.testdata = []
+        headermap = {0: 'code', 1: 'lat', 2: 'lng', 3: 'latLo', 4: 'lngLo', 5: 'latHi', 6: 'longHi'}
+        tests_fn = 'test_data/encodingTests.csv'
+        with open(tests_fn, "r") as fin:
+            for line in fin:
+                if line.startswith('#'):
+                    continue
+                td = line.strip().split(',')
+                assert len(td) == len(headermap), 'Wrong format of testing data: {0}'.format(line)
+                # all values should be numbers except the code
+                for i in range(1, len(headermap)):
+                    td[i] = float(td[i])
+                self.testdata.append({headermap[i]: v for i, v in enumerate(td)})
+
+    def test_encoding(self):
+        for td in self.testdata:
+            codelength = len(td['code']) - 1
+            if '0' in td['code']:
+                codelength = td['code'].index("0")
+            self.assertEqual(td['code'], olc.encode(td['lat'], td['lng'], codelength), td)
+
+    def test_decoding(self):
+        precision = 10
+        for td in self.testdata:
+            decoded = olc.decode(td['code'])
+            self.assertEqual(round(decoded.latitudeLo, precision), round(td['latLo'], precision), td)
+            self.assertEqual(round(decoded.longitudeLo, precision), round(td['lngLo'], precision), td)
+            self.assertEqual(round(decoded.latitudeHi, precision), round(td['latHi'], precision), td)
+            self.assertEqual(round(decoded.longitudeHi, precision), round(td['longHi'], precision), td)
+
 
 if __name__ == '__main__':
     unittest.main()
