dec91c165c18d13448188bcd2b941fdb3e15b1dc
Isaac J <joh12041@umn.edu>
2017-06-30 14:37:14 -0500

    add coding header

--- python/openlocationcode.py
+++ python/openlocationcode.py
@@ -1,3 +1,5 @@
+#  -*- coding: utf-8 -*-
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
