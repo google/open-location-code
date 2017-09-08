1f49c15b5c918eed66cca345a2d334c7f773ad9b
Doug Rinckes <drinckes@google.com>
2017-09-08 13:31:13 +0200

    Update openlocationcode_example.cc

--- cpp/openlocationcode_example.cc
+++ cpp/openlocationcode_example.cc
@@ -2,11 +2,9 @@
 
 #include <iostream>
 
-using namespace std;
-
 int main() {
   // Encodes latitude and longitude into a Plus+Code.
-  string code = openlocationcode::Encode({47.0000625, 8.0000625});
+  std::string code = openlocationcode::Encode({47.0000625, 8.0000625});
   // => "8FVC2222+22"
 
   // Encodes latitude and longitude into a Plus+Code with a preferred length.
@@ -15,33 +13,36 @@ int main() {
 
   // Decodes a Plus+Code back into coordinates.
   openlocationcode::CodeArea code_area = openlocationcode::Decode(code);
-  // cout.precision(15);
-  // cout << code_area.GetLatitudeLo() << endl; // 47.000062496
-  // cout << code_area.GetLongitudeLo() << endl; // 8.00006250000001
-  // cout << code_area.GetLatitudeHi() << endl; // 47.000062504
-  // cout << code_area.GetLongitudeHi() << endl; // 8.0000625305176
-  // cout << code_area.GetCodeLength() << endl; // 16
+  cout << "Code length: " << cout.precision(15) << endl;
+  cout << code_area.GetLatitudeLo() << endl;   // 47.000062496
+  cout << code_area.GetLongitudeLo() << endl;  // 8.00006250000001
+  cout << code_area.GetLatitudeHi() << endl;   // 47.000062504
+  cout << code_area.GetLongitudeHi() << endl;  // 8.0000625305176
+  cout << code_area.GetCodeLength() << endl;   // 16
 
   // Checks if a Plus+Code is valid.
   bool isValid = openlocationcode::IsValid(code);
+  cout << "Is valid? " << isValid << endl;
   // => true
 
   // Checks if a Plus+Code is full.
   bool isFull = openlocationcode::IsFull(code);
+  cout << "Is full? " << isFull << endl;
   // => true
 
   // Checks if a Plus+Code is short.
   bool isShort = openlocationcode::IsShort(code);
+  cout << "Is short? " << isShort << endl;
   // => false
 
   // Shorten a Plus+Codes if possible by the given reference latitude and
   // longitude.
-  string short_code =
+  std::string short_code =
       openlocationcode::Shorten("9C3W9QCJ+2VX", {51.3708675, -1.217765625});
   // => "CJ+2VX"
 
   // Extends a Plus+Code by the given reference latitude and longitude.
-  string recovered_code =
+  std::string recovered_code =
       openlocationcode::RecoverNearest("CJ+2VX", {51.3708675, -1.217765625});
   // => "9C3W9QCJ+2VX"
 }
