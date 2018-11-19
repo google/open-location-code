3c8c8beee95333326fb466d8b33c5ed5ed024f61
Gonzalo Diethelm <gonzalo.diethelm@gmail.com>
2018-11-19 02:38:35 +0100

    Gonzus/cpp example output (#229)
    * Make our lives easier by ignoring some files
    
    * Fix example output and comments

--- cpp/openlocationcode_example.cc
+++ cpp/openlocationcode_example.cc
@@ -1,24 +1,29 @@
 #include "openlocationcode.h"
 
 #include <iostream>
+#include <iomanip>
 
 int main() {
   // Encodes latitude and longitude into a Plus+Code.
   std::string code = openlocationcode::Encode({47.0000625, 8.0000625});
+  std::cout << "Encoded: " << code << std::endl;
   // => "8FVC2222+22"
 
   // Encodes latitude and longitude into a Plus+Code with a preferred length.
   code = openlocationcode::Encode({47.0000625, 8.0000625}, 16);
-  // => "8FVC2222+22GCCCCC"
+  std::cout << "Encoded 16: " << code << std::endl;
+  // => "8FVC2222+22GCCCC"
 
   // Decodes a Plus+Code back into coordinates.
   openlocationcode::CodeArea code_area = openlocationcode::Decode(code);
-  std::cout << "Code length: " << std::cout.precision(15) << std::endl;
-  std::cout << code_area.GetLatitudeLo() << std::endl;   // 47.000062496
-  std::cout << code_area.GetLongitudeLo() << std::endl;  // 8.00006250000001
-  std::cout << code_area.GetLatitudeHi() << std::endl;   // 47.000062504
-  std::cout << code_area.GetLongitudeHi() << std::endl;  // 8.0000625305176
-  std::cout << code_area.GetCodeLength() << std::endl;   // 16
+  std::cout << "Code length: "
+            << std::fixed << std::setprecision(15)
+            << ' ' << code_area.GetLatitudeLo()    // 47.000062479999997
+            << ' ' << code_area.GetLongitudeLo()   // 8.000062500000013
+            << ' ' << code_area.GetLatitudeHi()    // 47.000062520000000
+            << ' ' << code_area.GetLongitudeHi()   // 8.000062622070317
+            << ' ' << code_area.GetCodeLength()    // 15
+            << std::endl;
 
   // Checks if a Plus+Code is valid.
   bool isValid = openlocationcode::IsValid(code);
@@ -39,10 +44,12 @@ int main() {
   // longitude.
   std::string short_code =
       openlocationcode::Shorten("9C3W9QCJ+2VX", {51.3708675, -1.217765625});
+  std::cout << "Shortened: " << short_code << std::endl;
   // => "CJ+2VX"
 
   // Extends a Plus+Code by the given reference latitude and longitude.
   std::string recovered_code =
       openlocationcode::RecoverNearest("CJ+2VX", {51.3708675, -1.217765625});
+  std::cout << "Recovered: " << recovered_code << std::endl;
   // => "9C3W9QCJ+2VX"
 }
