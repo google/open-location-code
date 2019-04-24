7f13c186677e7545fda23a3928d57c2e7f7fcfa5
Doug Rinckes <drinckes@google.com>
2019-04-24 14:44:43 +0200

    Verbose logging, force test to fail

--- js/test/gulpfile.js
+++ js/test/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var phantom = require('node-qunit-phantomjs');
 
 function test(callback) {
-  phantom('./test.html', {}, (result) => {
+  phantom('./test.html', {'verbose': true}, (result) => {
     // Called with 0 for successful test completion, 1 for failure(s).
     if (result === 0) {
       callback();
