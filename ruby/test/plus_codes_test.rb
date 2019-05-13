5d5b736d18f38442cde300d216151f07f70e45ff
Doug Rinckes <drinckes@google.com>
2019-05-13 11:31:37 +0200

    Update ruby library to new algorithm (#330)
    * fix ruby algorithm, add benchmark tests, update ruby version

--- ruby/test/plus_codes_test.rb
+++ ruby/test/plus_codes_test.rb
@@ -2,9 +2,10 @@ require 'test/unit'
 require_relative '../lib/plus_codes/open_location_code'
 
 class PlusCodesTest < Test::Unit::TestCase
-
   def setup
-    @test_data_folder_path = File.join(File.dirname(__FILE__), '..', '..', 'test_data')
+    @test_data_folder_path = File.join(
+      File.dirname(__FILE__), '..', '..', 'test_data'
+    )
     @olc = PlusCodes::OpenLocationCode.new
   end
 
@@ -18,7 +19,8 @@ class PlusCodesTest < Test::Unit::TestCase
       is_valid_olc = @olc.valid?(code)
       is_short_olc = @olc.short?(code)
       is_full_olc = @olc.full?(code)
-      result = is_valid_olc == is_valid && is_short_olc == is_short && is_full_olc == is_full
+      result = (is_valid_olc == is_valid && is_short_olc == is_short &&
+        is_full_olc == is_full)
       assert(result)
     end
   end
@@ -28,18 +30,19 @@ class PlusCodesTest < Test::Unit::TestCase
       cols = line.split(',')
       code_area = @olc.decode(cols[0])
       assert_equal(cols[1].to_i, code_area.code_length, 'Also should be equal')
-      assert((code_area.south_latitude - cols[2].to_f).abs < 0.001, 'South')
-      assert((code_area.west_longitude - cols[3].to_f).abs < 0.001, 'West')
-      assert((code_area.north_latitude - cols[4].to_f).abs < 0.001, 'North')
-      assert((code_area.east_longitude - cols[5].to_f).abs < 0.001, 'East')
+      # Check returned coordinates are within 1e-10 of expected.
+      precision = 1e-10
+      assert((code_area.south_latitude - cols[2].to_f).abs < precision, 'South')
+      assert((code_area.west_longitude - cols[3].to_f).abs < precision, 'West')
+      assert((code_area.north_latitude - cols[4].to_f).abs < precision, 'North')
+      assert((code_area.east_longitude - cols[5].to_f).abs < precision, 'East')
     end
   end
 
   def test_encode
     read_csv_lines('encoding.csv').each do |line|
-      if line.length == 0
-        next
-      end
+      next if line.length.zero?
+
       cols = line.split(',')
       code = @olc.encode(cols[0].to_f, cols[1].to_f, cols[2].to_i)
       assert_equal(cols[3], code)
@@ -54,11 +57,11 @@ class PlusCodesTest < Test::Unit::TestCase
       lng = cols[2].to_f
       short_code = cols[3]
       test_type = cols[4]
-      if test_type == 'B' || test_type == 'S'
+      if %w[B S].include?(test_type)
         short = @olc.shorten(code, lat, lng)
         assert_equal(short_code, short)
       end
-      if test_type == 'B' || test_type == 'R'
+      if %w[B R].include?(test_type)
         expanded = @olc.recover_nearest(short_code, lat, lng)
         assert_equal(code, expanded)
       end
@@ -67,7 +70,7 @@ class PlusCodesTest < Test::Unit::TestCase
   end
 
   def test_longer_encoding_with_special_case
-    assert_equal('CFX3X2X2+X2RRRRJ', @olc.encode(90.0, 1.0, 15));
+    assert_equal('CFX3X2X2+X2RRRRR', @olc.encode(90.0, 1.0, 15))
   end
 
   def test_exceptions
@@ -96,9 +99,38 @@ class PlusCodesTest < Test::Unit::TestCase
     assert(!@olc.valid?('3W00CJJJ+'))
   end
 
-  def read_csv_lines(csv_file)
-    f = File.open(File.join(@test_data_folder_path, csv_file), 'r')
-    f.each_line.lazy.select { |line| line !~ /^\s*#/ }.map { |line| line.chop }
+  def test_benchmark
+    test_data = []
+    100000.times do
+      exp = 10.0**rand(10)
+      lat = ((rand * 180 - 90) * exp).round / exp
+      lng = ((rand * 360 - 180) * exp).round / exp
+      len = rand(15)
+      len = rand(1..5) * 2 if len <= 10
+
+      test_data.push([lat, lng, len, @olc.encode(lat, lng, len)])
+    end
+    start_micros = (Time.now.to_f * 1e6).to_i
+    test_data.each do |lat, lng, len, _|
+      @olc.encode(lat, lng, len)
+    end
+    duration_micros = (Time.now.to_f * 1e6).to_i - start_micros
+    printf("Encode benchmark: %d usec total, %d loops, %f usec per call\n",
+           duration_micros, test_data.length,
+           duration_micros.to_f / test_data.length)
+
+    start_micros = (Time.now.to_f * 1e6).to_i
+    test_data.each do |_, _, _, code|
+      @olc.decode(code)
+    end
+    duration_micros = (Time.now.to_f * 1e6).to_i - start_micros
+    printf("Decode benchmark: %d usec total, %d loops, %f usec per call\n",
+           duration_micros, test_data.length,
+           duration_micros.to_f / test_data.length)
   end
 
+  def read_csv_lines(csv_file)
+    f = File.open(File.join(@test_data_folder_path, csv_file), 'r')
+    f.each_line.lazy.reject { |line| line =~ /^\s*#/ }.map(&:chop)
+  end
 end
