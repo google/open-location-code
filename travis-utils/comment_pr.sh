8d0fb442eb67f5d8dc073bb402036a80e8da77a5
Doug Rinckes <drinckes@google.com>
2019-05-13 14:33:07 +0200

    strip colour controls

--- travis-utils/comment_pr.sh
+++ travis-utils/comment_pr.sh
@@ -6,6 +6,8 @@
 STDIN=`cat`
 # Echo it out so it gets caught by the TravisCI test log.
 echo "$STDIN"
+# Remove bash colour characters or GitHub's comment JSON parser complains.
+STDIN_CLEAN=`echo $STDIN | sed -r "s/\x1B\[[0-9]+m//g"`
 
 if [ -z "$TRAVIS_PULL_REQUEST" ]; then
   # We're not even in TravisCI AFAICT.
@@ -13,7 +15,11 @@ if [ -z "$TRAVIS_PULL_REQUEST" ]; then
 fi
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   # We're in a pull request.
-  curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"$STDIN\"}" \
-    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+  STATUS=`curl -s -o /dev/null -w "%{http_code}" \
+    -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    -d "{\"body\": \"$STDIN_CLEAN\"}" \
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"`
+  if [ "$STATUS" != "200" ]; then
+    echo -e "\e[31mFailed sending comment to GitHub\e[30m"
+  fi
 fi
