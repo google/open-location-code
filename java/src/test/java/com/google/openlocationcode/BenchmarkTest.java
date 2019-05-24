5085de22d33f38709d7128803a6a91f9310b6c34
Doug Rinckes <drinckes@google.com>
2019-05-24 12:42:34 +0200

    formatting

--- java/src/test/java/com/google/openlocationcode/BenchmarkTest.java
+++ java/src/test/java/com/google/openlocationcode/BenchmarkTest.java
@@ -3,21 +3,19 @@ package com.google.openlocationcode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** Tests encoding and decoding between Open Location Code and latitude/longitude pair. */
+/** Benchmark the encode and decode methods. */
 @RunWith(JUnit4.class)
 public class BenchmarkTest {
 
   public static final int LOOPS = 1000000;
-  
+
   public static Random generator = new Random();
-  
+
   private static class TestData {
 
     private final double latitude;
@@ -54,8 +52,10 @@ public class BenchmarkTest {
       OpenLocationCode.encode(testData.latitude, testData.longitude, testData.length);
     }
     long microsecs = (System.nanoTime() - start) / 1000;
-    
-    System.out.printf("Encode %d loops in %d usecs, %.3f usec per call\n", LOOPS, microsecs, (double)microsecs / LOOPS);
+
+    System.out.printf(
+        "Encode %d loops in %d usecs, %.3f usec per call\n",
+        LOOPS, microsecs, (double) microsecs / LOOPS);
   }
 
   @Test
@@ -65,7 +65,9 @@ public class BenchmarkTest {
       OpenLocationCode.decode(testData.code);
     }
     long microsecs = (System.nanoTime() - start) / 1000;
-    
-    System.out.printf("Decode %d loops in %d usecs, %.3f usec per call\n", LOOPS, microsecs, (double)microsecs / LOOPS);
+
+    System.out.printf(
+        "Decode %d loops in %d usecs, %.3f usec per call\n",
+        LOOPS, microsecs, (double) microsecs / LOOPS);
   }
 }
