d86f84dd5641bdbfac56760daa2bb209f0296a32
Doug Rinckes <drinckes@google.com>
2025-05-16 14:04:01 +0200

    fix rust formatting error

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,4 +6,7 @@ mod interface;
 mod private;
 
 pub use codearea::CodeArea;
-pub use interface::{decode, encode, encode_integers, is_full, is_short, is_valid, point_to_integers, recover_nearest, shorten};
+pub use interface::{
+    decode, encode, encode_integers, is_full, is_short, is_valid, point_to_integers,
+    recover_nearest, shorten,
+};
