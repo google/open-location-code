83986da0156bbf51fba33d0327d8ca4b7f955c89
Doug Rinckes <drinckes@google.com>
2025-06-20 15:48:13 +0200

    vb: use floor for conversion, add integer tests (#737)
    * vb: use floor for conversion, add integer tests
    
    * Add test instructions to README
    
    * Convert rounding to integer conversion, fix longitude wrap around.

--- visualbasic/README.md
+++ visualbasic/README.md
@@ -1,4 +1,3 @@
-
 # VBA Open Location Code Library
 
 This is an implementation of the Open Location Code library in VBA, Visual Basic
@@ -13,7 +12,7 @@ recovery to spreadsheet and other applications.
 
 ### Encoding
 
-```
+```vbnet
 OLCEncode(latitude, longitude [, length])
 ```
 
@@ -31,22 +30,22 @@ roughly 2x3 meters.
 Two decoding methods are provided. One returns a data structure, the other
 returns an array and is more suited to use within a spreadsheet.
 
-```
+```vbnet
 OLCDecode(code)
 ```
 
 This decodes the passed Open Location Code, and returns an `OLCArea` data
 structure, which has the following fields:
 
-* `latLo`: The latitude of the south-west corner of the code area.
-* `lngLo`: The longitude of the south-west corner of the code area.
-* `latCenter`: The latitude of the center of the code area.
-* `lngCenter`: The longitude of the center of the code area.
-* `latHi`: The latitude of the north-east corner of the code area.
-* `lngHi`: The longitude of the north-east corner of the code area.
-* `codeLength`: The number of digits in the code.
+- `latLo`: The latitude of the south-west corner of the code area.
+- `lngLo`: The longitude of the south-west corner of the code area.
+- `latCenter`: The latitude of the center of the code area.
+- `lngCenter`: The longitude of the center of the code area.
+- `latHi`: The latitude of the north-east corner of the code area.
+- `lngHi`: The longitude of the north-east corner of the code area.
+- `codeLength`: The number of digits in the code.
 
-```
+```vbnet
 OLCDecode2Array(code)
 ```
 
@@ -63,7 +62,7 @@ to Praia, Cape Verde.
 
 This works because 796RWF8Q+WF is the nearest match to the location.
 
-```
+```vbnet
 OLCShorten(code, latitude, longitude)
 ```
 
@@ -71,9 +70,9 @@ This removes as many digits from the code as possible, so that it is still the
 nearest match to the passed location.
 
 > Even if six or more digits can be removed, we suggest only removing four so
-that the codes are used consistently.
+> that the codes are used consistently.
 
-```
+```vbnet
 OLCRecoverNearest(code, latitude, longitude)
 ```
 
@@ -85,39 +84,48 @@ nearest matching full length code.
 > Tested using Microsoft Excel for Mac 2011 version 14.6.6
 
 1. Start Excel
-2. Select the menu option Tools > Macro > Visual Basic Editor
-3. After the project window opens, select the menu option File > Import File
-and import the `OpenLocationCode.bas` file. This will add the functions to the
-current workbook.
+1. Select the menu option Tools > Macro > Visual Basic Editor
+1. After the project window opens, select the menu option File > Import File
+   and import the `OpenLocationCode.bas` file. This will add the functions to the
+   current workbook.
 
 After importing, go back to the workbook, and run the self checks with:
 
 1. Select menu option Tools > Macro > Macros...
-2. In the Macro name: field type 'TestOLCLibrary' (it should be listed in the
-box) and click Run
-3. If successful, it will display a message window saying `All tests pass`
+1. In the Macro name: field type 'TestOLCLibrary' (it should be listed in the
+   box) and click Run
+1. If successful, it will display a message window saying `All tests pass`
 
 If `TestOLCLibrary` isn't listed, you may have imported the functions into
 another workbook.
 
 ## Loading Into OpenOffice/LibreOffice
 
-> Tested using LibreOffice version 4.2.8.2
+> Tested using LibreOffice version 25.2.2.2.
 
 To add the library to a OpenOffice or LibreOffice spreadsheet, follow these
 steps (this example uses LibreOffice):
 
-1. Select the menu option Tools > Macros > Organize Macros > LibreOffice Basic
-2. In the Macro From panel, select the spreadsheet to add the library to.
-3. Click New, enter a name for the module (e.g. OpenLocationCode), and press
-OK. It will then display the macro editor.
-4. Paste the full file into the editor, replacing the existing contents.
-5. Uncomment the line to enable VBA compatibility:
-```
-Option VBASupport 1
-```
-That's it. Save the file. You can now use the functions above in your
-spreadsheet!
+1. Select the menu option Tools > Macros > Organize Macros > Basic
+1. In the Macro From panel, select the spreadsheet to add the library to.
+1. Click New, enter a name for the module (e.g. OpenLocationCode), and press
+   OK. It will then display the macro editor.
+1. Paste the full `OpenLocationCode.bas` file into the editor, replacing the existing contents.
+1. Uncomment the line to enable VBA compatibility:
+
+   ```vbnet
+   Option VBASupport 1
+   ```
+
+   That's it. Save the file. You can now use the functions above in your
+   spreadsheet!
+
+## Running Tests
+
+If possible, run the `update_tests.sh` script, then paste the contents of the file `OLCTests.bas` into the end of your macros.
+
+Then you should be able to run the function `TEST_All`.
+This will run all the tests, and output either error messages or a message confirming success.
 
 ## Reporting Issues
 
---
+++ visualbasic/update_tests.sh
@@ -0,0 +1,221 @@
+#!/bin/bash
+set -e
+# Re-create the OLCTests.bas script using updated tests.
+
+VBA_TEST=OLCTests.bas
+if ! [ -f "$VBA_TEST" ]; then
+    echo "$VBA_TEST" must be in the current directory
+    exit 1
+fi
+
+# This function writes a VB function with the test cases from the file encoding.csv
+# Each line will add a test case to the array named in the first argument, and
+function addEncodingTests() {
+    TEST_CASE_COUNTER=0
+    STATEMENTS=""
+    while IFS=',' read -r latd lngd lati lngi len code || [[ -n "$code" ]]; do
+        # Skip lines that start with '#' (comments in the CSV file)
+        if [[ "$latd" =~ ^# ]]; then
+            continue
+        fi
+        # Skip empty lines
+        if [ -z "$latd" ]; then
+            continue
+        fi
+        STATEMENTS="$STATEMENTS    testCases(${TEST_CASE_COUNTER}) = Array(${latd}, ${lngd}, ${lati}, ${lngi}, ${len}, \"${code}\")\n"
+        TEST_CASE_COUNTER=$((TEST_CASE_COUNTER+1))
+    done < ../test_data/encoding.csv
+    # VB uses 0-based indexing for the array.
+    TEST_CASE_MAX_INDEX=$((TEST_CASE_COUNTER-1))
+
+    # Add the VB function.
+    echo -e "Private Function loadEncodingTestCSV() AS Variant\n\n    Dim testCases(${TEST_CASE_COUNTER}) As Variant" >>"$VBA_TEST"
+    # Add all the statments that populate the test data array.
+    echo -e "${STATEMENTS}" >>"$VBA_TEST"
+    echo -e "    loadEncodingTestCSV = testCases\nEnd Function" >>"$VBA_TEST"
+
+    # Add tests that use the encoding CSV data.
+    cat <<EOF >>"$VBA_TEST"
+
+' Check the degrees to integer conversions.
+' Due to floating point precision limitations, we may get values 1 less than expected.
+Sub TEST_IntegerConversion()
+    Dim encodingTests As Variant
+    Dim i As Integer
+    Dim tc As Variant
+    Dim degrees AS Double
+    Dim got_integer As Double
+    Dim want_integer As Double
+
+    encodingTests = loadEncodingTestCSV()
+
+    For i = 0 To ${TEST_CASE_MAX_INDEX}
+        tc = encodingTests(i)
+        degrees = tc(0)
+        want_integer = tc(2)
+        got_integer = latitudeToInteger(degrees)
+        If got_integer < want_integer - 1 Or got_integer > want_integer Then
+            MsgBox ("Encoding test " + CStr(i) + ": latitudeToInteger(" + CStr(degrees) + "): got " + CStr(got_integer) + ", want " + CStr(want_integer))
+            Exit Sub
+        End If
+        degrees = tc(1)
+        want_integer = tc(3)
+        got_integer = longitudeToInteger(degrees)
+        If got_integer < want_integer - 1 Or got_integer > want_integer Then
+            MsgBox ("Encoding test " + CStr(i) + ": longitudeToInteger(" + CStr(degrees) + "): got " + CStr(got_integer) + ", want " + CStr(want_integer))
+            Exit Sub
+        End If
+    Next
+
+    MsgBox ("TEST_IntegerConversion passes")
+End Sub
+
+' Check the integer encoding.
+Sub TEST_IntegerEncoding()
+    Dim encodingTests As Variant
+    Dim i As Integer
+    Dim tc As Variant
+    Dim latitude As Double
+    Dim longitude As Double
+    Dim code_length As Integer
+    Dim want_code As String
+    Dim got_code As String
+
+    encodingTests = loadEncodingTestCSV()
+
+    For i = 0 To ${TEST_CASE_MAX_INDEX}
+        tc = encodingTests(i)
+        ' Latitude and longitude are the integer values, not degrees.
+        latitude = tc(2)
+        longitude = tc(3)
+        code_length = tc(4)
+        want_code = tc(5)
+        got_code = encodeIntegers(latitude, longitude, code_length)
+        If got_code <> want_code Then
+            MsgBox ("Encoding test " + CStr(i) + ": encodeIntegers(" + CStr(latitude) + ", " + CStr(longitude) + ", " + CStr(code_length) + "): got " + got_code + ", want " + want_code)
+            Exit Sub
+        End If
+    Next
+
+    MsgBox ("TEST_IntegerEncoding passes")
+End Sub
+EOF
+}
+
+cat <<EOF >"$VBA_TEST"
+' Code to test the VisualBasic OpenLocationCode functions.
+' Copy this into your VB macro and run the TEST_All() function.
+
+EOF
+
+addEncodingTests
+
+# Now add the test functions.
+cat <<EOF >>"$VBA_TEST"
+' This is a subroutine to test the functions of the library, using test data
+' copied from the Github project. This should be migrated to being generated
+' from the CSV files.
+Sub TEST_OLCLibrary()
+  Dim i As Integer
+  Dim c As String
+  Dim a As OLCArea
+
+  Dim validity(17) As Variant
+  ' Fields code,isValid,isShort,isFull
+  validity(0) = Array("8fwc2345+G6", "true", "false", "true")
+  validity(1) = Array("8FWC2345+G6G", "true", "false", "true")
+  validity(2) = Array("8fwc2345+", "true", "false", "true")
+  validity(3) = Array("8FWCX400+", "true", "false", "true")
+  validity(4) = Array("WC2345+G6g", "true", "true", "false")
+  validity(5) = Array("2345+G6", "true", "true", "false")
+  validity(6) = Array("45+G6", "true", "true", "false")
+  validity(7) = Array("+G6", "true", "true", "false")
+  validity(8) = Array("G+", "false", "false", "false")
+  validity(9) = Array("+", "false", "false", "false")
+  validity(10) = Array("8FWC2345+G", "false", "false", "false")
+  validity(11) = Array("8FWC2_45+G6", "false", "false", "false")
+  validity(12) = Array("8FWC2η45+G6", "false", "false", "false")
+  validity(13) = Array("8FWC2345+G6+", "false", "false", "false")
+  validity(14) = Array("8FWC2300+G6", "false", "false", "false")
+  validity(15) = Array("WC2300+G6g", "false", "false", "false")
+  validity(16) = Array("WC2345+G", "false", "false", "false")
+  For i = 0 To 16
+    Dim v, s, f As Boolean
+    v = OLCIsValid(validity(i)(0))
+    s = OLCIsShort(validity(i)(0))
+    f = OLCIsFull(validity(i)(0))
+    If v <> (validity(i)(1) = "true") Then
+      MsgBox ("IsValid test " + CStr(i) + ", expected: " + CStr(validity(i)(1) = "true") + ", actual: " + CStr(v))
+      Exit Sub
+    End If
+    If s <> (validity(i)(2) = "true") Then
+      MsgBox ("IsShort test " + CStr(i) + ", expected: " + CStr(validity(i)(2) = "true") + ", actual: " + CStr(s))
+      Exit Sub
+    End If
+    If f <> (validity(i)(3) = "true") Then
+      MsgBox ("IsFull test " + CStr(i) + ", expected: " + CStr(validity(i)(3) = "true") + ", actual: " + CStr(f))
+      Exit Sub
+    End If
+  Next
+
+  Dim shortCodes(11) As Variant
+  shortCodes(0) = Array("9C3W9QCJ+2VX", "+2VX")
+  shortCodes(1) = Array("9C3W9QCJ+2VX", "CJ+2VX")
+  shortCodes(2) = Array("9C3W9QCJ+2VX", "CJ+2VX")
+  shortCodes(3) = Array("9C3W9QCJ+2VX", "CJ+2VX")
+  shortCodes(4) = Array("9C3W9QCJ+2VX", "CJ+2VX")
+  shortCodes(5) = Array("9C3W9QCJ+2VX", "9QCJ+2VX")
+  shortCodes(6) = Array("9C3W9QCJ+2VX", "9QCJ+2VX")
+  shortCodes(7) = Array("9C3W9QCJ+2VX", "9QCJ+2VX")
+  shortCodes(8) = Array("9C3W9QCJ+2VX", "9QCJ+2VX")
+  shortCodes(9) = Array("8FJFW222+", "22+")
+  shortCodes(10) = Array("796RXG22+", "22+")
+  Dim shortCoordinates(11) As Variant
+  shortCoordinates(0) = Array(51.3701125, -1.217765625)
+  shortCoordinates(1) = Array(51.3708675, -1.217765625)
+  shortCoordinates(2) = Array(51.3693575, -1.217765625)
+  shortCoordinates(3) = Array(51.3701125, -1.218520625)
+  shortCoordinates(4) = Array(51.3701125, -1.217010625)
+  shortCoordinates(5) = Array(51.3852125, -1.217765625)
+  shortCoordinates(6) = Array(51.3550125, -1.217765625)
+  shortCoordinates(7) = Array(51.3701125, -1.232865625)
+  shortCoordinates(8) = Array(51.3701125, -1.202665625)
+  shortCoordinates(9) = Array(42.899, 9.012)
+  shortCoordinates(10) = Array(14.95125, -23.5001)
+  For i = 0 To 10
+    c = OLCShorten(shortCodes(i)(0), shortCoordinates(i)(0), shortCoordinates(i)(1))
+    If c <> shortCodes(i)(1) Then
+      MsgBox ("Shorten test " + CStr(i) + ", expected: " + shortCodes(i)(1) + ", actual: " + c)
+      Exit Sub
+    End If
+    c = OLCRecoverNearest(shortCodes(i)(1), shortCoordinates(i)(0), shortCoordinates(i)(1))
+    If c <> shortCodes(i)(0) Then
+      MsgBox ("Recover test " + CStr(i) + ", expected: " + shortCodes(i)(0) + ", actual: " + c)
+      Exit Sub
+    End If
+  Next
+
+  ' North pole recovery test.
+  c = OLCRecoverNearest("2222+22", 89.6, 0.0)
+  If c <> "CFX22222+22" Then
+    MsgBox ("North pole recovery test, expected: CFX22222+22, actual: " + c)
+    Exit Sub
+  End If
+  ' South pole recovery test.
+  c = OLCRecoverNearest("XXXXXX+XX", -81.0, 0.0)
+  If c <> "2CXXXXXX+XX" Then
+    MsgBox ("South pole recovery test, expected: 2CXXXXXX+XX, actual: " + c)
+    Exit Sub
+  End If
+
+  MsgBox ("TEST_OLCLibrary passes")
+End Sub
+
+' Main test function. This will call the other test functions one by one.
+Sub TEST_All()
+    TEST_OLCLibrary
+
+    TEST_IntegerConversion
+    TEST_IntegerEncoding
+End Sub
+EOF
